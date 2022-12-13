3edc0451f56294db265882818e503e39a07a4ef1
DenisM <mdr2010@yandex.ru>
2022-12-14 00:00:44 +0500

    add Example 003 Sum

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -6,3 +6,10 @@ int numberC = 31;
 int numberD = 5;
 int result = numberC + numberD;
 Console.WriteLine(result);
+
+int numberE = new Random().Next(1,10); // 1 2 3...9 Случайные числа от 1 до 10
+Console.Write(numberE);
+int numberF = new Random().Next(1,10);
+Console.WriteLine(numberF);
+int sult = numberE + numberF;
+Console.WriteLine(sult);
