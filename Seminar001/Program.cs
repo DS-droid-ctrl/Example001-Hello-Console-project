6487ae9ab719b1e3393f2ba8612a1e2e1c04d65f
DenisM <mdr2010@yandex.ru>
2022-12-17 14:06:00 +0500

    add seminar1

---
+++ Seminar001/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+
+//Console.WriteLine("Введите число: ");
+
+//int number = Convert.ToInt32(Console.ReadLine());
+// int number = int.Parse(Console.ReadLine());
+
+//int sqr = number * number;
+
+//Console.Write("Квадрат числа: " + number + " равен: " + sqr);
+// либо Console.Write($"Квадрат числа {number} равен: {sqr}");
+
+//Console.WriteLine(sqr);
+
+// либо Console.Write("Квадрат числа: " + sqr + "еще что-нибудь");
+
+Console.Write("Введите первое число: ");
+int numFirst = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numSecond = Convert.ToInt32(Console.ReadLine());
+
+if (numFirst == numSecond*numSecond)
+{
+    Console.Write($"Число {numFirst} является квадратом {numSecond}");
+}
+else 
+    Console.Write($"Число {numFirst} не является квадратом {numSecond}");
