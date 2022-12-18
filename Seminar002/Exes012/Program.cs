899dbe9ddae95a66e7cb07c450ee7b2d1e986561
DenisM <mdr2010@yandex.ru>
2022-12-18 13:17:52 +0500

    add exes012

---
+++ Seminar002/Exes012/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a%b == 0)
+{
+    Console.Write($"Число {b} является кратным {a}");
+}
+else 
+    Console.Write($"Число {b} не является кратным {a}");
