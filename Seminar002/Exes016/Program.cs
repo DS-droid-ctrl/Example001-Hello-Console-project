f9f34d18171938a3cc70e77e166ea738c9bd7053
DenisM <mdr2010@yandex.ru>
2022-12-18 13:18:22 +0500

    add exes016

---
+++ Seminar002/Exes016/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.Write("Введите первое число: ");
+int numFirst = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numSecond = Convert.ToInt32(Console.ReadLine());
+
+if ((numFirst == numSecond*numSecond) || (numSecond == numFirst*numFirst))
+{
+    Console.Write($"Одно из чисел является квадратом другого");
+}
+else 
+    Console.Write($"Ни одно число не являются квадратом другого");
