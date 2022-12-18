1f03530118f186f589c6230d705a37490b6ab3e5
DenisM <mdr2010@yandex.ru>
2022-12-18 13:18:09 +0500

    add exes014

---
+++ Seminar002/Exes014/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if ((a%7 == 0) & (a%23 == 0))
+{
+    Console.Write($"Число {a} является кратным 7 и 23");
+}
+else 
+    Console.Write($"Число {a} не является кратным 7 и 23");
